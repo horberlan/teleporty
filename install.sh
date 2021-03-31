64fccc7c468759c981e775e4ec574d9355067bf6
horberlan <46747395+horberlan@users.noreply.github.com>
2021-03-31 14:35:24 -0300

    + Pink theme

--- install.sh
+++ install.sh
@@ -5,13 +5,16 @@ read -p "Do you want to install Teleport Theme [Y/n]?" asr
 case "$asr" in
     [yY][eE][sS]|[yY]|"")
 
-    echo "Copying Dark and Light theme theme..."
-    cp -Ri Teleport-theme-Dark-red $HOME/.local/share/plank/themes &&
-    cp -Ri Teleport-theme-Light-Blue $HOME/.local/share/plank/themes
+    echo "Copying themes..."
+    cp -Ri /Teleport-theme-Dark-red $HOME/.local/share/plank/themes &&
+    cp -Ri /Teleport-theme-Light-Blue $HOME/.local/share/plank/themes &&
+    cp -Ri /Teleport-theme-Pink $HOME/.local/share/plank/themes
+
     echo "Done"
 
     ;;
 *)
-esac   
+esac
+ #----------End here-----------#
 
 plank --preferences
